2de254534547aa0ecc1509ad07bb9eb48119a81e
Brent <blthiewes@gmail.com>
2019-10-01 20:55:14 -0500

    add author quiz

---
+++ authorquiz/src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AuthorQuiz.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function AuthorQuiz() {
+  return (
+    <div >
+      Author Quiz
+    </div>
+  );
+}
+
+export default AuthorQuiz;
