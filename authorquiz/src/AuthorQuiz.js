e8039c8052bd8f5c70e6b23f86bc69f161942e3e
Brent Thiewes <brent.thiewes@cambriausa.com>
2019-10-07 15:22:51 -0500

    finish up module 4

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,11 +1,67 @@
-import React from 'react';
-import './AuthorQuiz.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./AuthorQuiz.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function AuthorQuiz() {
+function Hero() {
   return (
-    <div >
-      Author Quiz
+    <div className="row">
+      <div className="jumbotron col-10 offeset-1">
+        <h1>AuthorQuiz</h1>
+        <p>Select the book writeen by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Book({title}) {
+  return <div className="answer">
+    <h4>{title}</h4>
+  </div>;
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        <img
+          src={author.imageUrl}
+          className="authorimage"
+          alt="author"
+        ></img>
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key ={title}></Book>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return <div></div>;
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from{" "}
+          <a href="http://commons.wikimedia.org/wiki/Main" > here</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData}/>
+      <Continue />
+      <Footer />
     </div>
   );
 }
--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -1,10 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AuthorQuiz from './AuthorQuiz';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AuthorQuiz from "./AuthorQuiz";
+import * as serviceWorker from "./serviceWorker";
+import { shuffle, sample } from "underscore";
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Twain1909.jpg/220px-Twain1909.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Adventures of Huckleberry Finn",
+      "Life on the Mississippi",
+      "Roughing It"
+    ]
+  },
+  {
+    name: "J. K. Rowling",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/220px-J._K._Rowling_2010.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "Harry Potter and the Philosopher's Stone",
+      "Fantastic Beasts and Where to Find Them",
+      "The Casual Vacancy"
+    ]
+  },
+  {
+    name: "Stephen King",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Stephen_King%2C_Comicon.jpg/220px-Stephen_King%2C_Comicon.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "Carrie",
+      "It",
+      "The Dark Tower"
+    ]
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Dickens_Gurney_head.jpg/200px-Dickens_Gurney_head.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "A Christmas Carol",
+      "David Copperfield ",
+      "Oliver Twist"
+    ]
+  }
+];
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function(p, c, i) {
+    return p.concat(c.books);
+  }, []);
+
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer))
+  };
+}
+
+const state = {
+  turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
