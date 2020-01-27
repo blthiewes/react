33316135a6a010972091c81b41ba1a629ad39109
Brent <blthiewes@gmail.com>
2020-01-26 20:04:16 -0600

    get the react app working in the server side rendered app

--- advanced-react/lib/components/Index.js
+++ advanced-react/lib/components/Index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  <h2>Hello React</h2>;
+  return <h2>Hello React</h2>;
 };
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
