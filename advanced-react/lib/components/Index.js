2a68afaff2f4b73520e9646addd80ad9709c9a90
Brent <blthiewes@gmail.com>
2020-01-26 20:30:27 -0600

    get async await with javascript using babel-polyfill

--- advanced-react/lib/components/Index.js
+++ advanced-react/lib/components/Index.js
@@ -1,9 +1,26 @@
 //starting point for react app
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <h2>Hello React</h2>;
-};
+import React, {Component} from 'react';
+
+class App extends Component {
+  state = {
+    answer: 45,
+  };
+
+  asyncFunc = () => {
+    return Promise.resolve(37);
+  };
+
+  async componentDidMount() {
+    this.setState({
+      answer: await this.asyncFunc(),
+    });
+  }
+
+  render() {
+    return <h2>Hello Class Component -- {this.state.answer}</h2>;
+  }
+}
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
--- advanced-react/webpack.config.js
+++ advanced-react/webpack.config.js
@@ -5,13 +5,13 @@
 const path = require('path');
 
 const config = {
-  entry: './lib/components/Index.js',
+  entry: ['babel-polyfill', './lib/components/Index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
-    rules: [{test: /\.js$/, use: 'babel-loader'}],
+    rules: [{test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}],
   },
 };
 
